70eaafeff0b5d0daf6aa8f6e6d2e75e5fe067103
Roman <lighttolight228@gmail.com>
2024-04-28 10:49:16 +0300

    first

--- models.py
+++ models.py
@@ -1,15 +1,18 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
+import datetime
+import jwt
+from flask import current_app
 
-from app import db
+db = SQLAlchemy()
 
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     login = db.Column(db.String(20), unique=True)
     email = db.Column(db.String(50), unique=True)
-    password = db.Column(db.String(128))  # Увеличил длину поля для пароля
+    password = db.Column(db.String(128))
     
     images = db.relationship('Image', back_populates='user', lazy=True)
 
@@ -23,10 +26,24 @@ class User(db.Model, UserMixin):
     def check_password(self, password):
         return check_password_hash(self.password, password)
 
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=expires_in)},
+            current_app.config['SECRET_KEY'], algorithm='HS256')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, current_app.config['SECRET_KEY'], algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return User.query.get(id)
+
 class Image(db.Model):
     __tablename__ = 'images'
     id = db.Column(db.Integer, primary_key=True)
-    filename = db.Column(db.String(255), nullable=False)
+    filename = db.Column(db.String(255), nullable=False)  # Уникальное имя файла на сервере
+    original_filename = db.Column(db.String(255), nullable=False)  # Оригинальное имя файла
     path = db.Column(db.String(255), nullable=False)
     image_metadata = db.Column(db.String(255))
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
@@ -35,3 +52,11 @@ class Image(db.Model):
 
     def __repr__(self):
         return '<Image %r>' % self.id
+
+    def __init__(self, filename, original_filename, path, metadata, user_id):
+        self.filename = filename
+        self.original_filename = original_filename
+        self.path = path
+        self.metadata = metadata
+        print (metadata)
+        self.user_id = user_id
--- static/js/canvas.js
+++ static/js/canvas.js
@@ -1,6 +1,7 @@
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
-let fileInput = document.getElementById("file-input");
+let fileInput = document.getElementById("file");
+let image;
 
 fileInput.addEventListener("change", loadImage);
 
@@ -10,11 +11,21 @@ function loadImage() {
     let reader = new FileReader();
 
     reader.onload = function (e) {
-        let image = new Image();
+        image = new Image();
         image.onload = function () {
             canvas.width = image.width;
             canvas.height = image.height;
             ctx.drawImage(image, 0, 0);
+            updateImage(); // Вызываем функцию обновления изображения
+            document.querySelector('.image-editor').style.display = 'block';
+            document.querySelector('.reset-button').style.display = 'block';
+            document.querySelector('.image-editor').classList.remove('hide'); // Удаляем класс hide
+            document.querySelector('.reset-button').classList.remove('hide'); // Удаляем класс hide
+            document.querySelector('.BODY_1').classList.add('hide');
+            document.querySelector('.BODY_2').classList.add('hide');
+            document.querySelector('.image-selection').classList.add('hide'); // Добавляем класс hide
+            document.querySelector('.controls').classList.remove('hide');
+
         }
         image.src = e.target.result;
     }
@@ -25,17 +36,29 @@ function loadImage() {
 // Обновляем фильтры изображения
 function updateImage() {
     let contrast = document.getElementById('contrast-slider').value;
+    document.getElementById('contrast-value').textContent = contrast;
+    
+    // Обновляем значение яркости
     let brightness = document.getElementById('brightness-slider').value;
+    document.getElementById('brightness-value').textContent = brightness;
+    
+    // Обновляем значение насыщенности
     let saturation = document.getElementById('saturation-slider').value;
+    document.getElementById('saturation-value').textContent = saturation;
+    
+    // Обновляем значение резкости
     let sharpness = document.getElementById('sharpness-slider').value;
+    document.getElementById('sharpness-value').textContent = sharpness;
 
-    let filters = `contrast(${contrast}) brightness(${brightness}) saturate(${saturation}) blur(${sharpness}px)`;
-    document.getElementById('image').style.filter = filters;
+    // Рисуем изображение с применением фильтров
+    ctx.filter = `contrast(${contrast}) brightness(${brightness}) saturate(${saturation}) blur(${sharpness}px)`;
+    ctx.drawImage(image, 0, 0); // Убираем передачу размеров
+    ctx.filter = 'none'; // Сбрасываем фильтры
 }
 
 // Сброс формы
 function resetForms() {
-    document.getElementById("file-input").value = "";
+    document.getElementById("file").value = "";
     document.getElementById("contrast-slider").value = 1;
     document.getElementById("brightness-slider").value = 1;
     document.getElementById("saturation-slider").value = 1;
@@ -44,3 +67,7 @@ function resetForms() {
     location.reload();
 }
 
+document.querySelector('form').addEventListener('reset', function() {
+    document.querySelector('.file').value = ''; // Сброс значения input типа file
+    document.querySelector('.image-editor').style.display = 'none'; // Скрываем image-editor div при сбросе формы
+});
